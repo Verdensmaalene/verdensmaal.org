7dabe55e63bcd369efaa4257b63bfadc61859047
Carl Törnqvist <carl@tornqv.ist>
2018-08-16 10:17:30 +0200

    Transform meta values during ssr

--- stores/meta.js
+++ stores/meta.js
@@ -7,17 +7,16 @@ function meta (state, emitter, app) {
 
   emitter.on('meta', function (next) {
     if (next.title !== state.title) emitter.emit('DOMTitleChange', next.title)
-    Object.assign(state.meta, next)
-
-    if (typeof window === 'undefined') return
 
     var url = ROOT + state.href
     var tags = Object.assign({'og:url': url}, next)
     if (next.title && !next['og:title']) tags['og:title'] = next.title
 
-    Object.keys(tags).forEach(key => {
+    Object.keys(tags).forEach(function (key) {
+      state.meta[key] = tags[key].replace(/^\//, ROOT + '/')
+      if (typeof window === 'undefined') return
       var el = document.head.querySelector(`meta[property="${key}"]`)
-      if (el) el.setAttribute('content', tags[key].replace(/^\//, ROOT + '/'))
+      if (el) el.setAttribute('content', state.meta[key])
     })
   })
 }
