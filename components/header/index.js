b7ac04a71ec1050ec9ae77cffeb10e91a0b4c93e
Anton Trollbäck <anton@trollback.se>
2018-09-07 10:28:31 +0200

    Make header content flexible (rem)

--- components/header/index.js
+++ components/header/index.js
@@ -109,29 +109,10 @@ module.exports = class Header extends Component {
                   <li class="Header-item">${item()}</li>
                 ` : html`
                   <li class="Header-item">
-                    <a class="${className('Header-button Header-button--link', { 'is-current': item.href.replace(/\/$/, '') === href, 'Header-button--external': item.external })}" target="${item.external ? '_blank' : '_self'}" rel="${item.external ? 'noopener noreferrer' : ''}" href="${item.href}">
+                    <a class="Header-button Header-button--link ${item.href.replace(/\/$/, '') === href ? 'is-current' : ''}" target="${item.external ? '_blank' : '_self'}" rel="${item.external ? 'noopener noreferrer' : ''}" href="${item.href}">
                       ${item.title}
-                      ${item.external ? html`
-                        <div>
-                          <svg class="Header-external" width="20" height="20" viewBox="0 0 20 20">
-                            <g fill="currentColor" transform="translate(.5 .5)">
-                              <rect width="1.25" height="15.385" x="17.61" y=".1"/>
-                              <polygon points="10.59 -6.92 11.84 -6.92 11.84 8.36 10.59 8.36" transform="rotate(90 11.215 .72)"/>
-                              <polygon points="8.646 -2.626 9.896 -2.626 9.896 22.013 8.646 22.013" transform="rotate(45 9.27 9.694)"/>
-                            </g>
-                          </svg>
-                          <svg class="Header-external Header-external--small" viewBox="0 0 13 13">
-                            <g fill-rule="evenodd">
-                              <path d="M12 0h1v9h-1z"/>
-                              <path d="M13 0v1H4V0z"/>
-                              <path d="M12.1.2l.7.7L1.3 12.4l-.7-.7z"/>
-                            </g>
-                          </svg>
-                          ${item.description ? html`<span class="Header-tooltip">${text`Go to ${item.description}`}</span>` : null}
-                        </div>
-                      ` : html`
-                        <div class="Header-arrow"></div>
-                      `}
+                      ${item.external ? html`<span class="Header-tooltip">${item.description ? text`Go to ${item.description}` : text`Opens in new tab`}</span>` : null}
+                      <div class="Header-arrow"></div>
                     </a>
                   </li>
                 `)}
