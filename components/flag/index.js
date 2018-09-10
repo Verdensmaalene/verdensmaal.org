12fa7ec5dc51897986cd6651402467f582171a2f
Anton Trollbäck <anton@trollback.se>
2018-09-10 16:08:42 +0200

    Link all the flags!

--- components/flag/index.js
+++ components/flag/index.js
@@ -7,15 +7,30 @@ module.exports = class Flag extends Component {
   }
 
   createElement (opts = {}) {
-    return html`
-      <div class="Flag ${opts.vertical ? 'Flag--vertical' : ''}">
-        <div class="Flag-figure">${opts.figure}</div>
-        <div class="Flag-body">
-          <strong class="Flag-title">${opts.title}</strong>
-          <span class="u-hiddenVisually"> – </span>
-          <span class="Flag-sub">${opts.text}</span>
-        </div>
+    var className = `Flag ${opts.vertical ? 'Flag--vertical' : ''}`
+
+    var body = html`
+      <div class="Flag-body">
+        <strong class="Flag-title">${opts.title}</strong>
+        <span class="u-hiddenVisually"> – </span>
+        <span class="Flag-sub">${opts.text}</span>
       </div>
     `
+
+    if (opts.href) {
+      return html`
+        <a class="${className}" href="${opts.href}">
+          <div class="Flag-figure">${opts.figure}</div>
+          ${body}
+        </a>
+      `
+    } else {
+      return html`
+        <div class="${className}">
+          <div class="Flag-figure">${opts.figure}</div>
+          ${body}
+        </div>
+      `
+    }
   }
 }
