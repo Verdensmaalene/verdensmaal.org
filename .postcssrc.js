75977793e1f83faed7cdece3ed2dba827d7dcedd
Carl Törnqvist <carl@tornqv.ist>
2018-10-08 18:39:46 +0200

    Exclude borked postcss plugins

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,7 +3,12 @@ module.exports = config
 function config (ctx) {
   var plugins = [require('postcss-focus-visible')]
   if (ctx.env !== 'development') {
-    plugins.push(require('postcss-custom-properties'))
+    plugins.push(
+      // TODO: include once https://github.com/postcss/postcss-custom-properties/pull/147 is released
+      // require('postcss-custom-properties'),
+      // TODO: include once https://github.com/MoOx/reduce-css-calc/issues/50 is resolved
+      // require('postcss-calc')
+    )
   }
 
   return { plugins }
