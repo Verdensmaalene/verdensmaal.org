5f620c64f46db6aca46db74550807200db11da03
Carl Törnqvist <carl@tornqv.ist>
2018-12-13 09:44:20 +0100

    Add support for svg images

--- components/text/serialize.js
+++ components/text/serialize.js
@@ -27,9 +27,16 @@ function serialize (type, node, content, children) {
       let sizes = [400, 600, 800, 1200].map(function (size, index) {
         return Math.min(size, node.dimensions.width * (index + 1))
       })
+      let src = node.url
+      let attrs = { alt: node.alt || '' }
+      if (!/\.svg$/.test(node.url)) {
+        attrs.sizes = '39em'
+        attrs.srcset = srcset(node.url, sizes)
+        src = srcset(node.url, [800]).split(' ')[0]
+      }
       return html`
         <figure>
-          <img sizes="39em" srcset="${srcset(node.url, sizes)}" src="${srcset(node.url, [800]).split(' ')[0]}" alt="${node.alt || ''}">
+          <img ${attrs} src="${src}">
           ${node.copyright ? html`<figcaption><small class="Text-muted">${node.copyright}</small></figcaption>` : null}
         </figure>
       `
