9d51dd954d2585d298ff4ab260dd8870ba1512cd
Anton Trollbäck <anton@trollback.se>
2019-01-11 15:57:51 +0100

    Fix focus outline issue in production

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,7 +1,7 @@
 module.exports = config
 
 function config (ctx) {
-  var plugins = [require('postcss-focus-visible')]
+  var plugins = [require('postcss-focus-visible')({ preserve: false })]
   if (ctx.env !== 'development') {
     plugins.push(require('postcss-custom-properties'))
   }
