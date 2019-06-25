5f846f1c1f93bff27c4c55ff7c73d986f9cc3019
Carl Törnqvist <carl@tornqv.ist>
2019-06-25 15:34:29 +0200

    Failed fetch should throw

--- sw.js
+++ sw.js
@@ -68,7 +68,7 @@ self.addEventListener('fetch', function onfetch (event) {
         function onerror (err) {
           if (fallback) return fallback
           if (isSameOrigin && url.pathname === '/') return findCachedLayout()
-          return err
+          throw err
         }
       }
 
