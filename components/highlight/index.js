5c16889c4a787891be58197f6fdf1682cb28922e
Carl Törnqvist <carl@tornqv.ist>
2020-04-24 16:01:07 +0000

    Fix highlight line wrap in Firefox

--- components/highlight/index.js
+++ components/highlight/index.js
@@ -2,7 +2,7 @@ var html = require('choo/html')
 
 module.exports = highlight
 
-function highlight (text, modifier) {
+function highlight (text, modifier = '') {
   return html`
     <span class="Highlight ${modifier}">
       <span class="Highlight-text">${text}</span>
