95f769fa55efb180a94bdc79129fdc0ac2300de6
Michael Petter <mpetter007@gmail.com>
2022-08-11 10:26:47 +0200

    added comment

--- components/base/index.js
+++ components/base/index.js
@@ -96,10 +96,12 @@ function resolve (doc) {
       }
       // handle archived sector page Verdenstimen
       if (doc.id === 'XvH3FBAAACIA135o') return '/verdenstimen'
-      // Just throw an error for unknown page types
+
+      // Insted of breaking return a hash link.
       if(type === 'Document') {
         return '#'
       }
+      // Just throw an error for unknown page types
       throw new Error('Document not recognized')
     }
   }
